0942013e5e34cd6ee628ccbcc0541627cea9b747
Ramin <168584750+rameen45a@users.noreply.github.com>
2024-04-30 23:46:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+for (let i = 0; i <= 10; i++) {
+    //this is loops//
+    console.log(i);
+}
+for (let j = 1; j <= 10; j++) {
+    //this is loops//
+    console.log(j * 3);
+}
+let k = 0;
+while (k < 6) {
+    //this is loop//
+    console.log(k);
+    k++;
+}
